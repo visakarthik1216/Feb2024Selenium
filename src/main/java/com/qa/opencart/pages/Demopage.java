4626d922f2f5d5fa710967c5f7d1db3148ac01de
Visalakshi Karthikraj <vishalir1216@gmail.com>
2024-07-03 00:53:02 -0700

    changed code as per PR comments by visaTL

--- src/main/java/com/qa/opencart/pages/Demopage.java
+++ src/main/java/com/qa/opencart/pages/Demopage.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.By;
 public class Demopage {
 	
 	By demo=By.id("demo");
+	By change=By.id("changed");
 	
 	public void getDemo()
 	{
 		System.out.println("printing demo");
+	}
+	
+	public void change()
+	{
+		System.out.println("changes done");
 	}
 
 }

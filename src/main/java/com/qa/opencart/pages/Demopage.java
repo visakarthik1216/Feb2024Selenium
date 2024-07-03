f76fe1797c9f9165bd4ce8a1d8d12c6033700745
Visalakshi Karthikraj <vishalir1216@gmail.com>
2024-07-03 00:42:26 -0700

    adding demopage

---
+++ src/main/java/com/qa/opencart/pages/Demopage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class Demopage {
+	
+	By demo=By.id("demo");
+	
+	public void getDemo()
+	{
+		System.out.println("printing demo");
+	}
+
+}

e428b0e7ea657e2075f06e29f057de9f02ff72d8
Visalakshi Karthikraj <vishalir1216@gmail.com>
2024-07-09 20:14:42 -0700

    modified login page title

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class AppConstants {
 	
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
-	public static final String ACCOUNT_PAGE_TITLE ="My Account";
+	public static final String ACCOUNT_PAGE_TITLE ="My Account11";
 	public static final String LOGIN_PAGE_FRACTION_URL ="route=account/login";
 	
 	public static final String CONFIG_FILE_PATH="./src/test/resource/config/Config.Properties";

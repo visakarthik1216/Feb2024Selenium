0d62d0e98f9fb0d3306d19b0b581aaa47ac780f0
Visalakshi Karthikraj <vishalir1216@gmail.com>
2024-07-10 22:18:04 -0700

    modified app constant by changing account title

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AppConstants {
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNT_PAGE_TITLE ="My Account";
 	public static final String LOGIN_PAGE_FRACTION_URL ="route=account/login";
 	
